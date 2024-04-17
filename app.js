2e830944677642eb6a28fd45ce67b8c81079c03f
phoebechin09 <xchin02@qub.ac.uk>
2024-04-17 15:51:28 +0100

    added cards and expansions webpage, added some cards to cards page and linked test card from TCGdex API

--- app.js
+++ app.js
@@ -1,10 +1,45 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+// const fetch = require('node-fetch');
+
+const cardTemplate = {
+    category: null,
+    id: null,
+    illustrator: null,
+    image: null,
+    localId: null,
+    name: null,
+    rarity: null,
+    variants: {
+      firstEdition: false,
+      holo: false,
+      normal: false,
+      reverse: false,
+      wPromo: false,
+    },
+    dexId: null,
+    hp: null,
+    types: null,
+    evolveFrom: null,
+    stage: null,
+    abilities: null,
+    attacks: null,
+    weaknesses: null,
+    retreat: null,
+    legal: {
+      standard: false,
+      expanded: false,
+    },
+    energyType: null,
+  };
+
+
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
 
+// render login, signup and tradecard webpage
 app.get('/login', (req, res) => {
     res.render('login', { /* data */ });
 });
@@ -16,6 +51,42 @@ app.get('/signup', (req, res) => {
 app.get('/tradecard', (req, res) => {
     res.render('tradecard', { /* data */ });
 });
+
+
+
+app.get('/expansions', (req, res) => {
+    res.render('expansions', { /* data */ });
+});
+
+
+// ********** DRAFTS HERE ************ //
+
+
+app.get('/cards', async (req, res) => {
+    try {
+        let url = `https://api.tcgdex.net/v2/en/cards/swsh3-136`;
+        let response = await fetch(url); // Fetch card data from the API
+        let cardData = await response.json(); // Parse response body as JSON
+        res.render('cards', { cardData }); // Render the 'cards.ejs' template with cardData
+    } catch (error) {
+        console.error('Error fetching card data:', error);
+        res.status(500).send('Error fetching card data'); // Send an error response to the client
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// ********** DRAFTS HERE ************ //
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
